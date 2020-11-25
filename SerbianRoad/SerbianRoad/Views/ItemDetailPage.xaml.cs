4ca41299003d37171a07a7d959799ded3e55b40a
Jovan <Jovan@DESKTOP-JTIACNO>
2020-11-25 15:51:25 +0100

    First commit

---
+++ SerbianRoad/SerbianRoad/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SerbianRoad.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SerbianRoad.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
